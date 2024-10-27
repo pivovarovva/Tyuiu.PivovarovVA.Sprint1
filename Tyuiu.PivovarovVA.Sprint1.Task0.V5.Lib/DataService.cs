e19adc74bd430342680153ccd0eb786864d02232
Admin <Admin@DESKTOP-34SQLP8>
2024-10-28 01:57:46 +0500

    Task0

--- Tyuiu.PivovarovVA.Sprint1.Task0.V5.Lib/DataService.cs
+++ Tyuiu.PivovarovVA.Sprint1.Task0.V5.Lib/DataService.cs
@@ -1,8 +1,13 @@
-﻿namespace ISprint1.Task0.V5.Lib
+﻿namespace Tyuiu.PivovarovVA.Sprint1.Task0.V5.Lib
 {
-    public static class MathService
+    public interface ISprint1Task0V5
     {
-        public static double CalculateExpression()
+        double CalculateExpression();
+    }
+
+    public class MathService : ISprint1Task0V5
+    {
+        public double CalculateExpression()
         {
             return (1 + 2) * (1 + 9.0 / 3);
         }
