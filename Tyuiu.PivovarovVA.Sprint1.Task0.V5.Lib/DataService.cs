ea3e8cbe943eaece4b52c07b78a42db15c85520e
Admin <Admin@DESKTOP-34SQLP8>
2024-10-28 01:39:33 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.PivovarovVA.Sprint1.Task0.V5.Lib/DataService.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.PivovarovVA.Sprint1.Task0.V5.Lib
+{
+    public static class MathService
+    {
+        public static double CalculateExpression()
+        {
+            return (1 + 2) * (1 + 9.0 / 3);
+        }
+    }
+}
---
+++ Tyuiu.PivovarovVA.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+namespace Tyuiu.PivovarovVA.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public class MathServiceTest
+    {
+        [TestMethod]
+        public void TestCalculateExpression()
+        {
+            double expected = 12.0;
+            double result = MathService.CalculateExpression();
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    internal class TestClassAttribute : Attribute
+    {
+    }
+
+    internal class MathService
+    {
+        internal static double CalculateExpression()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class TestMethodAttribute : Attribute
+    {
+    }
+}
+
